4299d68f4e1358514ebaa6c89c4b89a382572187
Jerry Wang <ziyanwang1105@gmail.com>
2024-04-14 16:43:03 -0700

    finish routes and main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,41 @@
+
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { AuthRoute } from './components/Routes/Routes';
+import MainPage from './components/MainPage/MainPage';
+import LoginForm from './components/SessionForms/LoginForm';
+import SignupForm from './components/SessionForms/SignupForm';
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <AuthRoute component={MainPage} />
+      },
+      {
+        path: "login",
+        element: <AuthRoute component={LoginForm} />
+      },
+      {
+        path: "signup",
+        element: <AuthRoute component={SignupForm} />
+      }
+    ]
+  }
+]);
+
 function App() {
-  return <h1> Hello from App </h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+function MainPage() {
+    return (
+      <>
+        <p>A Twitter Clone</p>
+        <footer>
+          Copyright &copy; 2023 MERN Twitter
+        </footer>
+      </>
+    );
+  }
+
+  export default MainPage;

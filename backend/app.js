5632595bbf2b8d83652e81a6ea3441863c5ec60e
Jerry Wang <ziyanwang1105@gmail.com>
2024-04-14 12:34:30 -0700

    finish express routing

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,16 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require('cors')
+const {isProduction} = require('./config/keys')
+
+const csurf = require('csurf')
+
+const debug = require('debug')
+
 var usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
 
 var app = express();
 
@@ -12,8 +20,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!isProduction) {
 
-app.use('/users', usersRouter);
-app.use('/tweets', tweetsRouter)
+    app.use(cors());
+  }
+
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+);
+
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter)
+app.use('/api/csrf', csrfRouter)
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
 
 module.exports = app;
